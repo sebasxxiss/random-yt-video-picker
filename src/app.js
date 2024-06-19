a7c2d4aae921ec988af72d709eb2a29f078f429c
Sebas <sebas.rodas.m16@gmail.com>
2024-06-19 15:58:35 -0600

    modified:   src/app.js

--- src/app.js
+++ src/app.js
@@ -2,16 +2,23 @@ import express from "express";
 import router from "./routes.js";
 import cors from "cors";
 
-let origin = "";
+/*let origin = "";
 const port = process.env.PORT ?? 10000;
 if (process.env.NODE_ENV === "developmnet") {
   origin = `http://localhost:${port}`;
 } else {
   origin = "https://random-yt-video-picker.onrender.com";
-}
+}*/
 
 const app = express();
-app.use(cors({ origin: origin }));
+app.use(
+  cors({
+    origin: [
+      `http://localhost:${port}`,
+      "https://random-yt-video-picker.onrender.com",
+    ],
+  })
+);
 app.use(express.json());
 app.use("/", express.static("public"));
 app.use("/api", router);
