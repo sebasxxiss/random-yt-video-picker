03d7faa05c951343a607c4bdf9bacad25ee38ac1
Sebas <sebas.rodas.m16@gmail.com>
2024-06-19 15:16:07 -0600

    Add development and production mode:   src/app.js

--- src/app.js
+++ src/app.js
@@ -2,9 +2,16 @@ import express from "express";
 import router from "./routes.js";
 import cors from "cors";
 
-const app = express();
+let origin = "";
 const port = process.env.PORT ?? 10000;
-app.use(cors({ origin: `http://localhost:${port}` }));
+if (process.env.NODE_ENV === "developmnet") {
+  origin = `http://localhost:${port}`;
+} else {
+  origin = "https://random-yt-video-picker.onrender.com";
+}
+
+const app = express();
+app.use(cors({ origin: origin }));
 app.use(express.json());
 app.use("/", express.static("public"));
 app.use("/api", router);
