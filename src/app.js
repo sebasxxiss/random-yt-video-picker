ed31dcea171d78475b517298db2e09c0d70a24b6
Sebas <sebas.rodas.m16@gmail.com>
2024-06-19 15:01:26 -0600

    modified:   public/index.js 	modified:   src/app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import router from "./routes.js";
 import cors from "cors";
 
 const app = express();
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT ?? 10000;
 app.use(cors({ origin: `http://localhost:${port}` }));
 app.use(express.json());
 app.use("/", express.static("public"));
